99ff53f3659bfecaa7c02374675c752363d145d8
Anthony <asadbeksaydullayev2911@gmail.com>
2026-09-11 19:14:39 +0900

    feat: MUI customized integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+import { AppProps } from "next/app";
+import { useState } from "react";
+import { light } from "@/scss/MaterialTheme";
+
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  //@ts-ignore
+  const [theme, setTheme] = useState(createTheme(light))
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />;
+    </ThemeProvider>
+
+
+
+  )
 }
